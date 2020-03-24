5807f7f12b8a71fb8bf9f8c9ff82611f73e768bd
Andrei Dumitrescu <andrei_dumitrescu_96@yahoo.com>
2020-03-24 23:23:23 +0200

    fast fixes

--- back/resources/views/layouts/navbars/sidebar.blade.php
+++ back/resources/views/layouts/navbars/sidebar.blade.php
@@ -18,23 +18,23 @@
         </a>
       </li>
       <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
-        <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="true">
-          <i><img style="width:25px" src="{{ asset('material') }}/img/laravel.svg"></i>
-          <p>{{ __('Laravel Examples') }}
+        <a class="nav-link" data-toggle="collapse" href="#laravelExample" aria-expanded="{{ ($activePage == 'profile' || $activePage == 'user-management') ? 'true' : 'false' }}">
+          <i class="material-icons">supervisor_account</i>
+          <p>{{ __('User Management') }}
             <b class="caret"></b>
           </p>
         </a>
-        <div class="collapse show" id="laravelExample">
+        <div class="collapse {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' show' : '' }}" id="laravelExample">
           <ul class="nav">
             <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
               <a class="nav-link" href="{{ route('profile.edit') }}">
-                <span class="sidebar-mini"> UP </span>
+                <i class="material-icons">perm_identity</i>
                 <span class="sidebar-normal">{{ __('User profile') }} </span>
               </a>
             </li>
             <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
               <a class="nav-link" href="{{ route('user.index') }}">
-                <span class="sidebar-mini"> UM </span>
+                <i class="material-icons">settings</i>
                 <span class="sidebar-normal"> {{ __('User Management') }} </span>
               </a>
             </li>
@@ -42,23 +42,23 @@
         </div>
       </li>
       <li class="nav-item {{ ($activePage == 'elements' || $activePage == 'categories') ? ' active' : '' }}">
-        <a class="nav-link" data-toggle="collapse" href="#content" aria-expanded="true">
+        <a class="nav-link" data-toggle="collapse" href="#content" aria-expanded="{{ ($activePage == 'elements' || $activePage == 'categories') ? 'true' : 'false' }}">
           <i class="material-icons">settings_input_component</i>
           <p>{{ __('Components') }}
             <b class="caret"></b>
           </p>
         </a>
-        <div class="collapse show" id="content">
+        <div class="collapse {{ ($activePage == 'elements' || $activePage == 'categories') ? ' show' : '' }}" id="content">
           <ul class="nav">
             <li class="nav-item{{ $activePage == 'categories' ? ' active' : '' }}">
               <a class="nav-link" href="{{ route('category.index') }}">
-                <i class="material-icons">class</i>
+                <i class="material-icons">web_asset</i>
                 <span class="sidebar-normal"> {{ __('Categories') }} </span>
               </a>
             </li>
             <li class="nav-item{{ $activePage == 'elements' ? ' active' : '' }}">
               <a class="nav-link" href="{{ route('element.index') }}">
-                <i class="material-icons">dns</i>
+                <i class="material-icons">web</i>
                 <span class="sidebar-normal">{{ __('Elements') }} </span>
               </a>
             </li>
