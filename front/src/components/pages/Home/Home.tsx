4d999f3bf355a50494a8f9f5c13c156804ffbbee
Andrei Dumitrescu <andrei_dumitrescu_96@yahoo.com>
2020-05-20 21:47:40 +0300

    functional homepage

--- front/src/components/pages/Home/Home.tsx
+++ front/src/components/pages/Home/Home.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useEffect, useRef} from 'react';
 import Navbar from './../../UI/organisms/Navbar/Navbar';
 import Footer from './../../UI/organisms/Footer/Footer';
 
 import theme from './Home.module.scss';
 import SideText from './../../UI/atoms/SideText/SideText';
 import { currentPageType } from '../../../types/Utils/Utils';
-import {ReactComponent as LogoTopArrow} from '../../../assets/svg/logo_top_arrow.svg';
-import {ReactComponent as LogoBotArrow} from '../../../assets/svg/logo_bottom_arrow.svg';
-import {ReactComponent as LogoMidArrow} from '../../../assets/svg/logo_middle_arrow.svg';
+import {ReactComponent as Logo} from '../../../assets/svg/logo_full.svg';
+import { useDispatch } from 'react-redux';
+import { setCurrentPage } from '../../../actions/utils.action';
 
 const Home:React.FC = () => {
 
+    const swapPage = (e:React.MouseEvent<SVGSVGElement>) => {
+        console.log(e);
+    }
+    const logoRef = useRef<SVGSVGElement>(null);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(logoRef.current !== null){   
+            logoRef.current.childNodes.forEach((spline, index) => {
+
+                /**
+                 * since the order from top to bottom of elements is index = [2 0 1], in
+                 * order to animate them in order, we must look at them from a binary
+                 * point of view 
+                 */
+                (spline as HTMLElement).style.animationDelay = `${(index % 2 + 1 - index / 2)*0.7}s` ;
+                (spline as HTMLElement).onanimationend = () => {
+                    
+                    //skip the middle part
+                    if(index){
+                        (spline as HTMLElement).classList.add(theme.home__logo__splineHover)
+                    }
+                }
+                
+                spline.addEventListener('click', () => {
+                    switch (index){
+                        case 1:
+                            dispatch(setCurrentPage(currentPageType.category));
+                        break;
+                        case 2:
+                            dispatch(setCurrentPage(currentPageType.studio));
+                        break;
+                        default:
+                        break;
+                    }
+                })
+            });
+        }
+    },[])
 
     return( 
     
@@ -23,17 +62,10 @@ const Home:React.FC = () => {
                     position={'left'}
                 />
                 <div className={theme.home__container__logo}>
-                    <LogoTopArrow
+                    <Logo
                         className={theme.home__logo}
-                        onPointerEnter={() => console.log('top')}
-                    />
-                    <LogoMidArrow
-                        className={theme.home__logo}
-                        onPointerEnter={() => console.log('mid')}
-                    />
-                    <LogoBotArrow
-                        className={theme.home__logo}
-                        onPointerEnter={() => console.log('bot')}
+                        onClick={swapPage}
+                        ref={logoRef}
                     />
                 </div>
                 <SideText
